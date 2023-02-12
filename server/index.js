9948a3d171d4b601579eb40e2f6bca03ed840bb5
baspop01 <73678928+baspop01@users.noreply.github.com>
2023-02-12 14:39:10 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: 'CMBrfq45',
+    password: '',
     database: 'project_mobile',
 })
 
